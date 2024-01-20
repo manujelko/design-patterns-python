4e1236be3b5894e654d94c9eec8fc549b6548f9f
Manuel Šarić <manujelko@gmail.com>
2024-01-20 23:22:28 +0100

    Add singleton

---
+++ creational/singleton.py
@@ -0,0 +1,14 @@
+class Singleton:
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super(Singleton, cls).__new__(cls, *args, **kwargs)
+        return cls._instance
+
+
+if __name__ == "__main__":
+    s1 = Singleton()
+    s2 = Singleton()
+
+    print(f"s1 is s2: {s1 is s2}")

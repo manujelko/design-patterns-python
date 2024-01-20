ef9b50240793d99ca24fafd4acdef1e88b76fce6
Manuel Šarić <manujelko@gmail.com>
2024-01-20 23:35:24 +0100

    Add adapter pattern

---
+++ structural/adapter.py
@@ -0,0 +1,23 @@
+class EuropeanSocket:
+    def charge(self) -> str:
+        return "Charging with European socket"
+
+
+class USASocket:
+    def power(self) -> str:
+        return "Powering with USA socket"
+
+
+class SocketAdapter(EuropeanSocket):
+    def __init__(self, usa_socket: USASocket) -> None:
+        self.usa_socket = usa_socket
+
+    def charge(self) -> str:
+        power = self.usa_socket.power()
+        return f"Adapting {power} to European charging standard"
+
+
+if __name__ == "__main__":
+    usa_socket = USASocket()
+    adapter = SocketAdapter(usa_socket)
+    print(adapter.charge())
